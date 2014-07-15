927ae463d158ceeb17bcbcd224be910429f7718d
Alexander Nähring <alexander.naehring@rwth-aachen.de>
2014-07-15 18:38:15 +0200

    tuesday session
    RC 01

--- server.c
+++ server.c
@@ -17,6 +17,7 @@
 int main(void)  {
   char *buffer;
   size_t size;
+  int port;
   
   AS_version(); // not used here
   
@@ -29,21 +30,33 @@ int main(void)  {
     
     if(strstr(buffer, "help") == buffer || strstr(buffer, "h") == buffer) {
       printf("AS example server - help:\n");
-      printf("  help, h:    show this help\n");
-      printf("  quit, q:    quit server\n");
-      printf("  server, s:  print list of running server in this process\n");
+      printf("  help,  h:  show this help\n");
+      printf("  quit,  q:  quit program\n");
+      printf("  print, p:  print list of running server in this process\n");
+      printf("  start <port>, s <port>:  start a new server\n");
+      printf("  stop <port>,  t <port>:  stop a running server\n");
       //printf("  clients, c: print list of all connected clients for all running server in this process\n");
     }
     
-    if(strstr(buffer, "server") == buffer || strstr(buffer, "s") == buffer) {
+    if(strstr(buffer, "print") == buffer || strstr(buffer, "p") == buffer) {
       AS_ServerPrintRunning();
     }
     
     if(strstr(buffer, "quit") == buffer || strstr(buffer, "q") == buffer) { 
       free(buffer);
       break;  // break while loop
-    }
+    }   
+        
+    port = -1; sscanf(buffer, "start %d\n", &port);
+    if(port > -1) AS_ServerStart(port, AS_IPv6);
+    port = -1; sscanf(buffer, "s %d\n", &port);
+    if(port > -1) AS_ServerStart(port, AS_IPv6);
+    port = -1; sscanf(buffer, "stop %d\n", &port);
+    if(port > -1) AS_ServerStop(port);
+    port = -1; sscanf(buffer, "t %d\n", &port);
+    if(port > -1) AS_ServerStop(port);
+    
     free(buffer);
   }
-  AS_ServerStop(20144);
+  AS_ServerStop(0); // 0 = all
 }
